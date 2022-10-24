2b5d759e9b0b7e10030443e4e7c401858c847c7a
fshstk <fshstk@fshstk.com>
2022-10-24 06:35:45 +0200

    unit test

---
+++ test/SphericalVectorTest.cpp
@@ -0,0 +1,12 @@
+#include "utils/SphericalVector.h"
+#include <catch2/catch.hpp>
+
+TEST_CASE("Cartesian to spherical conversion should deliver correct result")
+{
+  FAIL("unimplemented");
+}
+
+TEST_CASE("Spherical to cartesian conversion should deliver correct result")
+{
+  FAIL("unimplemented");
+}
