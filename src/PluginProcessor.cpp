b9ccc78933724920b39efff2ae9db70179e10b3c
fshstk <fshstk@fshstk.com>
2022-11-11 00:09:12 +0100

    wip (unsure)

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -11,25 +11,29 @@ PluginProcessor::PluginProcessor()
 
 void PluginProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
+  juce::ignoreUnused(sampleRate);
   bufferCopy.setSize(2, samplesPerBlock);
 }
 
-void PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&)
 {
-  // COPY BUFFER:
-  const int numInputChannels = getTotalNumInputChannels() < 2 ? 1 : 2;
+  const auto maxInputChannels = 2;
+  const auto maxOutputChannels = 36;
+  const auto numInputChannels = std::min(getTotalNumInputChannels(), maxInputChannels);
+  const auto numOutputChannels = std::min(getTotalNumOutputChannels(), maxOutputChannels);
+
+  // EVALUATE COEFFICIENTS:
+  const auto halfWidth = juce::degreesToRadians(params.width() / 2);
+  const auto ypr = params.getYPR();
+  SHL_current = harmonics({ .azimuth = ypr.yaw - 0 * halfWidth, .elevation = ypr.pitch });
+  SHR_current = harmonics({ .azimuth = ypr.yaw + 0 * halfWidth, .elevation = ypr.pitch });
+
+  // COPY BUFFER (DOESN'T DEAL WITH MONO SIGNALS):
   for (int i = 0; i < numInputChannels; ++i)
     bufferCopy.copyFrom(i, 0, buffer.getReadPointer(i), buffer.getNumSamples());
 
-  // EVALUATE COEFFICIENTS:
-  const auto halfWidth = degreesToRadians(params.width() / 2);
-  const auto ypr = params.getYPR();
-  SHL_current = harmonics({ .azimuth = ypr.yaw - halfWidth, .elevation = ypr.pitch });
-  SHR_current = harmonics({ .azimuth = ypr.yaw + halfWidth, .elevation = ypr.pitch });
-
   // COPY OUTPUT INTO BUFFER, FADING BETWEEN PREVIOUS AND CURRENT COEFFICIENTS:
-  const auto numOutputChannels = juce::jmin(buffer.getNumChannels(), 36);
-  for (int i = 0; i < numOutputChannels; ++i) {
+  for (auto i = 0; i < numOutputChannels; ++i) {
     buffer.copyFromWithRamp(
       i, 0, bufferCopy.getReadPointer(0), buffer.getNumSamples(), SHL_old[i], SHL_current[i]);
     buffer.addFromWithRamp(
@@ -37,8 +41,8 @@ void PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiB
   }
 
   // COPY SHL/SHR INTO SHL_old/SHR_old:
-  juce::FloatVectorOperations::copy(&SHL_old[0], &SHL_current[0], numOutputChannels);
-  juce::FloatVectorOperations::copy(&SHR_old[0], &SHR_current[0], numOutputChannels);
+  juce::FloatVectorOperations::copy(&SHL_old[0], &SHL_current[0], 36);
+  juce::FloatVectorOperations::copy(&SHR_old[0], &SHR_current[0], 36);
 }
 
 juce::AudioProcessorEditor* PluginProcessor::createEditor()
