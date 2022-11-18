91de1f7ca7279ae2c7a49e18b99cb1ef26a2229e
fshstk <fshstk@fshstk.com>
2022-11-18 18:54:57 +0100

    AAA

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -2,14 +2,14 @@
 #include "Fonts.h"
 
 namespace {
-const int width = 415;
-const int height = 475;
-const juce::Colour background = { 48, 48, 48 };
-const juce::Colour foreground = { 218, 218, 218 };
-const auto mainFont =
-  juce::Typeface::createSystemTypefaceFor(BinaryData::JockeyOne_ttf, BinaryData::JockeyOne_ttfSize);
+const auto width = 415;
+const auto height = 475;
+const auto background = juce::Colour{ 48, 48, 48 };
+const auto foreground = juce::Colour{ 218, 218, 218 };
 const auto titleFont = juce::Typeface::createSystemTypefaceFor(BinaryData::JollyLodger_ttf,
                                                                BinaryData::JollyLodger_ttfSize);
+const auto bodyFont =
+  juce::Typeface::createSystemTypefaceFor(BinaryData::JockeyOne_ttf, BinaryData::JockeyOne_ttfSize);
 } // namespace
 
 PluginEditor::PluginEditor(PluginProcessor& p)
