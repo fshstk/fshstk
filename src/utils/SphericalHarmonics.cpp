e8d999e0baa80c068162275fb5a3a996b5e22bdb
fshstk <fshstk@fshstk.com>
2022-11-20 15:13:15 +0100

    define to inline func (BENCHMARK THIS)

--- src/utils/SphericalHarmonics.cpp
+++ src/utils/SphericalHarmonics.cpp
@@ -3,13 +3,28 @@
 #include <complex>
 
 using std::sqrt;
-constexpr auto pi = M_PI;
-
-#define SQ(n) (n * n)
-#define CUBE(n) (n * n * n)
-#define QUART(n) (n * n * n * n)
 
 namespace {
+constexpr auto pi = M_PI;
+
+template<typename T>
+inline T sq(T n)
+{
+  return n * n;
+}
+
+template<typename T>
+inline T cube(T n)
+{
+  return n * n * n;
+}
+
+template<typename T>
+inline T quart(T n)
+{
+  return n * n * n * n;
+}
+
 auto degreesToRadians(double deg)
 {
   return deg * pi / 180.0;
@@ -43,39 +58,39 @@ std::array<double, 36> harmonics_n3d(double x, double y, double z)
     // order 2:
     sqrt(15) * y * x / (2 * sqrt(pi)),
     sqrt(15) * y * z / (2 * sqrt(pi)),
-    sqrt(5) * (3 * SQ(z) - 1) / (4 * sqrt(pi)),
+    sqrt(5) * (3 * sq(z) - 1) / (4 * sqrt(pi)),
     sqrt(15) * x * z / (2 * sqrt(pi)),
-    sqrt(15) * (SQ(x) - SQ(y)) / (4 * sqrt(pi)),
+    sqrt(15) * (sq(x) - sq(y)) / (4 * sqrt(pi)),
     // order 3:
-    sqrt(2) * sqrt(35) * y * (3 * SQ(x) - SQ(y)) / (8 * sqrt(pi)),
+    sqrt(2) * sqrt(35) * y * (3 * sq(x) - sq(y)) / (8 * sqrt(pi)),
     sqrt(105) * y * x * z / (2 * sqrt(pi)),
-    sqrt(2) * sqrt(21) * y * (-1 + 5 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(7) * z * (5 * SQ(z) - 3) / (4 * sqrt(pi)),
-    sqrt(2) * sqrt(21) * x * (-1 + 5 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(105) * (SQ(x) - SQ(y)) * z / (4 * sqrt(pi)),
-    sqrt(2) * sqrt(35) * x * (SQ(x) - 3 * SQ(y)) / (8 * sqrt(pi)),
+    sqrt(2) * sqrt(21) * y * (-1 + 5 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(7) * z * (5 * sq(z) - 3) / (4 * sqrt(pi)),
+    sqrt(2) * sqrt(21) * x * (-1 + 5 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(105) * (sq(x) - sq(y)) * z / (4 * sqrt(pi)),
+    sqrt(2) * sqrt(35) * x * (sq(x) - 3 * sq(y)) / (8 * sqrt(pi)),
     // order 4:
-    3 * sqrt(35) * y * x * (SQ(x) - SQ(y)) / (4 * sqrt(pi)),
-    3 * sqrt(2) * sqrt(35) * y * (3 * SQ(x) - SQ(y)) * z / (8 * sqrt(pi)),
-    3 * sqrt(5) * y * x * (-1 + 7 * SQ(z)) / (4 * sqrt(pi)),
-    3 * sqrt(2) * sqrt(5) * y * z * (-3 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    3 * (35 * QUART(z) - 30 * SQ(z) + 3) / (16 * sqrt(pi)),
-    3 * sqrt(2) * sqrt(5) * x * z * (-3 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    3 * sqrt(5) * (SQ(x) - SQ(y)) * (-1 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    3 * sqrt(2) * sqrt(35) * x * (SQ(x) - 3 * SQ(y)) * z / (8 * sqrt(pi)),
-    3 * sqrt(35) * (QUART(x) - 6 * SQ(y) * SQ(x) + QUART(y)) / (16 * sqrt(pi)),
+    3 * sqrt(35) * y * x * (sq(x) - sq(y)) / (4 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(35) * y * (3 * sq(x) - sq(y)) * z / (8 * sqrt(pi)),
+    3 * sqrt(5) * y * x * (-1 + 7 * sq(z)) / (4 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(5) * y * z * (-3 + 7 * sq(z)) / (8 * sqrt(pi)),
+    3 * (35 * quart(z) - 30 * sq(z) + 3) / (16 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(5) * x * z * (-3 + 7 * sq(z)) / (8 * sqrt(pi)),
+    3 * sqrt(5) * (sq(x) - sq(y)) * (-1 + 7 * sq(z)) / (8 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(35) * x * (sq(x) - 3 * sq(y)) * z / (8 * sqrt(pi)),
+    3 * sqrt(35) * (quart(x) - 6 * sq(y) * sq(x) + quart(y)) / (16 * sqrt(pi)),
     // order 5:
-    3 * sqrt(2) * sqrt(77) * y * (5 * QUART(x) - 10 * SQ(y) * SQ(x) + QUART(y)) / (32 * sqrt(pi)),
-    3 * sqrt(385) * y * x * (SQ(x) - SQ(y)) * z / (4 * sqrt(pi)),
-    sqrt(2) * sqrt(385) * y * (3 * SQ(x) - SQ(y)) * (-1 + 9 * SQ(z)) / (32 * sqrt(pi)),
-    sqrt(1155) * y * x * z * (-1 + 3 * SQ(z)) / (4 * sqrt(pi)),
-    sqrt(165) * y * (-14 * SQ(z) + 21 * QUART(z) + 1) / (16 * sqrt(pi)),
-    sqrt(11) * z * (63 * QUART(z) - 70 * SQ(z) + 15) / (16 * sqrt(pi)),
-    sqrt(165) * x * (-14 * SQ(z) + 21 * QUART(z) + 1) / (16 * sqrt(pi)),
-    sqrt(1155) * (SQ(x) - SQ(y)) * z * (-1 + 3 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(2) * sqrt(385) * x * (SQ(x) - 3 * SQ(y)) * (-1 + 9 * SQ(z)) / (32 * sqrt(pi)),
-    3 * sqrt(385) * (QUART(x) - 6 * SQ(y) * SQ(x) + QUART(y)) * z / (16 * sqrt(pi)),
-    3 * sqrt(2) * sqrt(77) * x * (QUART(x) - 10 * SQ(y) * SQ(x) + 5 * QUART(y)) / (32 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(77) * y * (5 * quart(x) - 10 * sq(y) * sq(x) + quart(y)) / (32 * sqrt(pi)),
+    3 * sqrt(385) * y * x * (sq(x) - sq(y)) * z / (4 * sqrt(pi)),
+    sqrt(2) * sqrt(385) * y * (3 * sq(x) - sq(y)) * (-1 + 9 * sq(z)) / (32 * sqrt(pi)),
+    sqrt(1155) * y * x * z * (-1 + 3 * sq(z)) / (4 * sqrt(pi)),
+    sqrt(165) * y * (-14 * sq(z) + 21 * quart(z) + 1) / (16 * sqrt(pi)),
+    sqrt(11) * z * (63 * quart(z) - 70 * sq(z) + 15) / (16 * sqrt(pi)),
+    sqrt(165) * x * (-14 * sq(z) + 21 * quart(z) + 1) / (16 * sqrt(pi)),
+    sqrt(1155) * (sq(x) - sq(y)) * z * (-1 + 3 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(2) * sqrt(385) * x * (sq(x) - 3 * sq(y)) * (-1 + 9 * sq(z)) / (32 * sqrt(pi)),
+    3 * sqrt(385) * (quart(x) - 6 * sq(y) * sq(x) + quart(y)) * z / (16 * sqrt(pi)),
+    3 * sqrt(2) * sqrt(77) * x * (quart(x) - 10 * sq(y) * sq(x) + 5 * quart(y)) / (32 * sqrt(pi)),
   };
 }
 
@@ -94,39 +109,39 @@ std::array<double, 36> harmonics_sn3d(double x, double y, double z)
     // order 2 (1/sqrt(5)):
     sqrt(3) * y * x / (2 * sqrt(pi)),
     sqrt(3) * y * z / (2 * sqrt(pi)),
-    (3 * SQ(z) - 1) / (4 * sqrt(pi)),
+    (3 * sq(z) - 1) / (4 * sqrt(pi)),
     sqrt(3) * x * z / (2 * sqrt(pi)),
-    sqrt(3) * (SQ(x) - SQ(y)) / (4 * sqrt(pi)),
+    sqrt(3) * (sq(x) - sq(y)) / (4 * sqrt(pi)),
     // order 3 (1/sqrt(7)):
-    sqrt(10) * y * (3 * SQ(x) - SQ(y)) / (8 * sqrt(pi)),
+    sqrt(10) * y * (3 * sq(x) - sq(y)) / (8 * sqrt(pi)),
     sqrt(15) * y * x * z / (2 * sqrt(pi)),
-    sqrt(6) * y * (-1 + 5 * SQ(z)) / (8 * sqrt(pi)),
-    z * (5 * SQ(z) - 3) / (4 * sqrt(pi)),
-    sqrt(6) * x * (-1 + 5 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(15) * (SQ(x) - SQ(y)) * z / (4 * sqrt(pi)),
-    sqrt(10) * x * (SQ(x) - 3 * SQ(y)) / (8 * sqrt(pi)),
+    sqrt(6) * y * (-1 + 5 * sq(z)) / (8 * sqrt(pi)),
+    z * (5 * sq(z) - 3) / (4 * sqrt(pi)),
+    sqrt(6) * x * (-1 + 5 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(15) * (sq(x) - sq(y)) * z / (4 * sqrt(pi)),
+    sqrt(10) * x * (sq(x) - 3 * sq(y)) / (8 * sqrt(pi)),
     // order 4 (1/sqrt(9) = 1/3):
-    sqrt(35) * y * x * (SQ(x) - SQ(y)) / (4 * sqrt(pi)),
-    sqrt(2) * sqrt(35) * y * (3 * SQ(x) - SQ(y)) * z / (8 * sqrt(pi)),
-    sqrt(5) * y * x * (-1 + 7 * SQ(z)) / (4 * sqrt(pi)),
-    sqrt(2) * sqrt(5) * y * z * (-3 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    (35 * QUART(z) - 30 * SQ(z) + 3) / (16 * sqrt(pi)),
-    sqrt(2) * sqrt(5) * x * z * (-3 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(5) * (SQ(x) - SQ(y)) * (-1 + 7 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(2) * sqrt(35) * x * (SQ(x) - 3 * SQ(y)) * z / (8 * sqrt(pi)),
-    sqrt(35) * (QUART(x) - 6 * SQ(y) * SQ(x) + QUART(y)) / (16 * sqrt(pi)),
+    sqrt(35) * y * x * (sq(x) - sq(y)) / (4 * sqrt(pi)),
+    sqrt(2) * sqrt(35) * y * (3 * sq(x) - sq(y)) * z / (8 * sqrt(pi)),
+    sqrt(5) * y * x * (-1 + 7 * sq(z)) / (4 * sqrt(pi)),
+    sqrt(2) * sqrt(5) * y * z * (-3 + 7 * sq(z)) / (8 * sqrt(pi)),
+    (35 * quart(z) - 30 * sq(z) + 3) / (16 * sqrt(pi)),
+    sqrt(2) * sqrt(5) * x * z * (-3 + 7 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(5) * (sq(x) - sq(y)) * (-1 + 7 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(2) * sqrt(35) * x * (sq(x) - 3 * sq(y)) * z / (8 * sqrt(pi)),
+    sqrt(35) * (quart(x) - 6 * sq(y) * sq(x) + quart(y)) / (16 * sqrt(pi)),
     // order 5 (1/sqrt(11)):
-    3 * sqrt(14) * y * (5 * QUART(x) - 10 * SQ(y) * SQ(x) + QUART(y)) / (32 * sqrt(pi)),
-    3 * sqrt(35) * y * x * (SQ(x) - SQ(y)) * z / (4 * sqrt(pi)),
-    sqrt(70) * y * (3 * SQ(x) - SQ(y)) * (-1 + 9 * SQ(z)) / (32 * sqrt(pi)),
-    sqrt(105) * y * x * z * (-1 + 3 * SQ(z)) / (4 * sqrt(pi)),
-    sqrt(15) * y * (-14 * SQ(z) + 21 * QUART(z) + 1) / (16 * sqrt(pi)),
-    z * (63 * QUART(z) - 70 * SQ(z) + 15) / (16 * sqrt(pi)),
-    sqrt(15) * x * (-14 * SQ(z) + 21 * QUART(z) + 1) / (16 * sqrt(pi)),
-    sqrt(105) * (SQ(x) - SQ(y)) * z * (-1 + 3 * SQ(z)) / (8 * sqrt(pi)),
-    sqrt(70) * x * (SQ(x) - 3 * SQ(y)) * (-1 + 9 * SQ(z)) / (32 * sqrt(pi)),
-    3 * sqrt(35) * (QUART(x) - 6 * SQ(y) * SQ(x) + QUART(y)) * z / (16 * sqrt(pi)),
-    3 * sqrt(14) * x * (QUART(x) - 10 * SQ(y) * SQ(x) + 5 * QUART(y)) / (32 * sqrt(pi)),
+    3 * sqrt(14) * y * (5 * quart(x) - 10 * sq(y) * sq(x) + quart(y)) / (32 * sqrt(pi)),
+    3 * sqrt(35) * y * x * (sq(x) - sq(y)) * z / (4 * sqrt(pi)),
+    sqrt(70) * y * (3 * sq(x) - sq(y)) * (-1 + 9 * sq(z)) / (32 * sqrt(pi)),
+    sqrt(105) * y * x * z * (-1 + 3 * sq(z)) / (4 * sqrt(pi)),
+    sqrt(15) * y * (-14 * sq(z) + 21 * quart(z) + 1) / (16 * sqrt(pi)),
+    z * (63 * quart(z) - 70 * sq(z) + 15) / (16 * sqrt(pi)),
+    sqrt(15) * x * (-14 * sq(z) + 21 * quart(z) + 1) / (16 * sqrt(pi)),
+    sqrt(105) * (sq(x) - sq(y)) * z * (-1 + 3 * sq(z)) / (8 * sqrt(pi)),
+    sqrt(70) * x * (sq(x) - 3 * sq(y)) * (-1 + 9 * sq(z)) / (32 * sqrt(pi)),
+    3 * sqrt(35) * (quart(x) - 6 * sq(y) * sq(x) + quart(y)) * z / (16 * sqrt(pi)),
+    3 * sqrt(14) * x * (quart(x) - 10 * sq(y) * sq(x) + 5 * quart(y)) / (32 * sqrt(pi)),
   };
 }
 
