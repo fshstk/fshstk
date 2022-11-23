a4c868d43d6b9f22c0e1e7d980700082c0d08b2f
fshstk <fshstk@fshstk.com>
2022-11-23 21:26:50 +0100

    formatting

--- src/gui/PointToFloat.h
+++ src/gui/PointToFloat.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <juce_gui_basics/juce_gui_basics.h>
 
-// Convenience function since functions like
-// juce::Component::getLocalBounds().getCentre() return a juce::Point<int>, but
-// certain graphics functions in Juce expect floats instead.
+// Convenience function since functions like juce::Component::getLocalBounds().getCentre() return a
+// juce::Point<int>, but certain graphics functions in Juce expect floats instead.
 template<typename T>
 juce::Point<float> pointToFloat(const juce::Point<T> p)
 {
