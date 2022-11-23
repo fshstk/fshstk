b66c2e41bbc19b599108975305b712c1311afd0c
fshstk <fshstk@fshstk.com>
2022-11-23 18:11:31 +0100

    orderknob finished

--- src/gui/components/OrderKnob.cpp
+++ src/gui/components/OrderKnob.cpp
@@ -1,16 +1,57 @@
 #include "OrderKnob.h"
+#include "GuiGlobals.h"
+
+namespace {
+const auto orderRange = std::pair{ 0, 5 };
+const auto knobRangeDegrees = 120.0f;
+
+juce::Path orderNumber(int order, juce::Point<float> center)
+{
+  auto path = juce::Path{};
+  auto glyphs = juce::GlyphArrangement{};
+
+  const auto nSteps = orderRange.second - orderRange.first;
+  const auto angle = juce::degreesToRadians(knobRangeDegrees) *
+                     (static_cast<float>(order - orderRange.first) / (nSteps - 0) - 0.5f);
+
+  // Line width just needs to be a constant that's definitely big enough to fit
+  // the string we want to draw:
+  const auto lineWidth = 100.0f;
+
+  const auto radius = guiSizes::editorGridSize;
+  const auto margin = 7;
+
+  glyphs.addJustifiedText(guiFonts::body,
+                          juce::String{ order },
+                          center.getX() - lineWidth / 2.0f,
+                          center.getY(),
+                          lineWidth,
+                          juce::Justification::horizontallyCentred);
+  glyphs.createPath(path);
+
+  path.applyTransform(juce::AffineTransform{}
+                        .translated(0, -(radius + margin))
+                        .rotated(angle, center.getX(), center.getY()));
+  return path;
+}
+} // namespace
 
 OrderKnob::OrderKnob()
-  : SimpleKnob("order", 120.0)
+  : SimpleKnob("order", knobRangeDegrees)
 {
-  const auto minOrder = 0;
-  const auto maxOrder = 5;
-  setRange(minOrder, maxOrder, 1);
+  assert(orderRange.second > orderRange.first);
+  setRange(orderRange.first, orderRange.second, 1);
   setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, {}, {}, {});
 }
 
 void OrderKnob::paint(juce::Graphics& g)
 {
   SimpleKnob::paint(g);
-  //
+
+  const auto center = juce::Point{ static_cast<float>(getLocalBounds().getCentreX()),
+                                   static_cast<float>(getLocalBounds().getCentreY()) };
+
+  g.setColour(guiColors::foreground);
+  for (auto order = orderRange.first; order <= orderRange.second; ++order)
+    g.fillPath(orderNumber(order, center));
 }
