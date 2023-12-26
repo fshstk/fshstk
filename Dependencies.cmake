dd0351c5a94b85ec7b02f18af0d45f94120cd1e8
fshstk <fshstk@fshstk.com>
2023-12-26 02:44:28 +0100

    Remove unit testing (for now) and other dangling dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -30,40 +30,6 @@ set(VST_SDK_PATH
   ${juce_vst_SOURCE_DIR}/modules/juce_audio_processors/format_types/VST3_SDK)
 
 ################################################################################
-# KFR - DSP library
-################################################################################
-
-FetchContent_Declare(
-  KFR
-  GIT_REPOSITORY https://github.com/kfrlib/kfr
-  GIT_TAG        4.3.0)
-FetchContent_MakeAvailable(KFR)
-
-################################################################################
-# Catch2 - Library for unit testing
-################################################################################
-
-FetchContent_Declare(
-  Catch2
-  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-  GIT_TAG        v2.13.9)
-FetchContent_MakeAvailable(Catch2)
-
-list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
-include(Catch)
-
-################################################################################
-# Boost - All-purpose library that includes spherical harmonics
-################################################################################
-
-# FetchContent_Declare(
-#   Boost
-#   GIT_REPOSITORY https://github.com/boostorg/boost.git
-#   GIT_TAG        boost-1.80.0
-# )
-# FetchContent_MakeAvailable(Boost)
-
-################################################################################
 # FMT - C++ String Formatting
 ################################################################################
 
