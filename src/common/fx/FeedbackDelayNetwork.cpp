ec4752809e1146b8b8311322b8c372c272117dea
fshstk <fshstk@fshstk.com>
2024-01-08 03:38:57 +0100

    Add comment about possible FDN bug

--- src/common/fx/FeedbackDelayNetwork.cpp
+++ src/common/fx/FeedbackDelayNetwork.cpp
@@ -110,6 +110,7 @@ void FeedbackDelayNetwork::updateParameterSettings()
 
   for (auto channel = 0U; channel < fdnSize; ++channel) {
     // TODO: why divide by 10 to get better range for room size setting
+    // TODO: rarely throws EXC_BAD_ACCESS?
     const auto delayLengthMilliseconds = 0.1 * primeNumbers[primeIndices[channel]];
     const auto delayLengthSeconds = 0.001 * delayLengthMilliseconds;
     const auto delayLengthSamples = static_cast<size_t>(delayLengthSeconds * sampleRate);
