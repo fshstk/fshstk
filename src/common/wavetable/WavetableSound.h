a247db632c7d0f407ac7ddf2139843bd3fe7c9d3
fshstk <fshstk@fshstk.com>
2024-01-11 22:25:50 +0100

    Add fsh::stk header comments to C++ files

--- src/common/wavetable/WavetableSound.h
+++ src/common/wavetable/WavetableSound.h
@@ -1,3 +1,24 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic                software    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
 #pragma once
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_dsp/juce_dsp.h>
