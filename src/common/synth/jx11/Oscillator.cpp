4d9238bc0a3a3d60ddb717c256a9a29b6c88d76a
fshstk <fshstk@fshstk.com>
2024-01-16 20:54:50 +0100

    Add fsh::stk header comment to all JX11 files

--- src/common/synth/jx11/Oscillator.cpp
+++ src/common/synth/jx11/Oscillator.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic                software    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
 #include "Oscillator.h"
 #include "spdlog/spdlog.h"
 #include <cmath>
