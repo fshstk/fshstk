b26e4f28fcb6e35f96cabaebf85b28cb41f40b1d
fshstk <fshstk@fshstk.com>
2024-01-19 18:47:15 +0100

    Add docs to FloatParam/ChoiceParam

--- src/common/pluginbase/FloatParam.h
+++ src/common/pluginbase/FloatParam.h
@@ -23,17 +23,37 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 
 namespace fsh {
+/**
+ * Used to add a floating point parameter to a plugin.
+ *
+ * Use a designated initializer and call create() directly for maximum readability, e.g.:
+ * ```cpp
+ * fsh::FloatParam{
+ *   .id = "parameter_id",
+ *   .name = "The Name of the Parameter",
+ *   .range = { [min], [max] },
+ * }.create()
+ * ```
+ *
+ * Return a list of these inside a function returning a
+ * juce::AudioProcessorValueTreeState::ParameterLayout object to create the parameter layout, which
+ * you can then pass to the constructor of your plugin's PluginState class.
+ */
 struct FloatParam
 {
+  /// Used to specify the parameter's range. See the JUCE docs for details.
   using Range = juce::NormalisableRange<float>;
+
+  /// Used to specify the parameter's attributes, e.g. a label. See the JUCE docs for details.
   using Attributes = juce::AudioParameterFloatAttributes;
 
-  juce::ParameterID id;
-  juce::String name;
-  Range range;
-  float defaultVal = 0.0;
-  Attributes attributes = {};
+  juce::ParameterID id;   ///< The parameter's unique ID, used to identify it in the DAW
+  juce::String name;      ///< The parameter's name, displayed in the DAW's automation
+  Range range;            ///< The parameter's range, including optional step size and skew factor
+  float defaultVal = 0.0; ///< The parameter's default value
+  Attributes attributes = {}; ///< The parameter's attributes, e.g. a label
 
+  /// Creates a juce::AudioParameterFloat object from the given parameters
   auto create() const
   {
     return std::make_unique<juce::AudioParameterFloat>(id, name, range, defaultVal, attributes);
