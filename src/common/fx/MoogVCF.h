482da117a0a74e0681db420c9dd3dc14947fc893
fshstk <fshstk@fshstk.com>
2024-01-21 22:12:03 +0100

    Moog VCF filter stub

---
+++ src/common/fx/MoogVCF.h
@@ -0,0 +1,29 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic                software    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
+#pragma once
+
+namespace fsh::fx {
+class MoogVCF
+{
+  //
+};
+} // namespace fsh::fx
