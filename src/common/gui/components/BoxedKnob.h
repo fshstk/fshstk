3655944ccc71dda04e07d7d6c386afb1aa562100
fshstk <fshstk@fshstk.com>
2024-01-23 04:54:08 +0100

    BoxedKnob stub

---
+++ src/common/gui/components/BoxedKnob.h
@@ -0,0 +1,30 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic                software    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
+#pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
+
+namespace fsh::gui {
+class BoxedKnob : public juce::Component
+{
+  //
+};
+} // namespace fsh::gui
