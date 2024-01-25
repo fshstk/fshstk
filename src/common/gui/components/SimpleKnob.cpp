8781e814b3fb9bd162c6669ca49e7cecc6e1c966
fshstk <fshstk@fshstk.com>
2024-01-26 00:00:08 +0100

    Add USE_MATH_DEFINES definition for MSVC

--- src/common/gui/components/SimpleKnob.cpp
+++ src/common/gui/components/SimpleKnob.cpp
@@ -19,6 +19,7 @@
                                     www.gnu.org/licenses/gpl-3.0
 ***************************************************************************************************/
 
+#define _USE_MATH_DEFINES
 #include "SimpleKnob.h"
 #include <cmath>
 
