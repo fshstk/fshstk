6542fe288a2e4020efa9f383499952fb8b74d65d
fshstk <fshstk@fshstk.com>
2024-02-03 03:25:31 +0100

    Add Distortion class

---
+++ src/common/fx/Distortion.cpp
@@ -0,0 +1,34 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic               synthesis    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
+#include "Distortion.h"
+
+using namespace fsh::fx;
+
+void Distortion::setParams(const Params& params)
+{
+  _params = params;
+}
+
+auto Distortion::processSample(float x) const -> float
+{
+  return _params.function(x * _params.preGain) / _params.preGain;
+}
---
+++ src/common/fx/Distortion.h
@@ -0,0 +1,55 @@
+/***************************************************************************************************
+                 ██████          █████                              █████    █████
+                ███░░███        ░░███                              ░░███    ░░███
+               ░███ ░░░   █████  ░███████      ██    ██     █████  ███████   ░███ █████
+              ███████    ███░░   ░███░░███    ░░    ░░     ███░░  ░░░███░    ░███░░███
+             ░░░███░    ░░█████  ░███ ░███                ░░█████   ░███     ░██████░
+               ░███      ░░░░███ ░███ ░███                 ░░░░███  ░███ ███ ░███░░███
+               █████     ██████  ████ █████    ██    ██    ██████   ░░█████  ████ █████
+             ░░░░░     ░░░░░░  ░░░░ ░░░░░    ░░    ░░    ░░░░░░     ░░░░░  ░░░░ ░░░░░
+
+            fantastic  spatial  holophonic               synthesis    tool    kit
+
+                                    copyright (c) fabian hummel
+                                       www.github.com/fshstk
+                                           www.fshstk.com
+
+         this file is part of the fantastic spatial holophonic software toolkit (fsh::stk)
+  fsh::stk is free software: it is provided under the terms of the gnu general public license v3.0
+                                    www.gnu.org/licenses/gpl-3.0
+***************************************************************************************************/
+
+#pragma once
+#include <functional>
+
+namespace fsh::fx {
+/**
+Distortion/modulation effect class.
+
+This class provides a simple distortion effect, which can be used to add harmonics to a signal. You
+can set the pre-gain and the distortion function, which is applied to the signal. By default, an
+atan function is used, which is a simple and effective way to add harmonics to a signal.
+*/
+class Distortion
+{
+public:
+  /// The parameters for the distortion effect
+  struct Params
+  {
+    /// Linear gain multiplier, which is applied to the input signal (and reversed afterwards)
+    float preGain = 1.0f;
+
+    /// Distortion function which is applied to the signal
+    std::function<float(float)> function = [](float x) { return std::atan(x); };
+  };
+
+  /// Sets the parameters for the distortion effect
+  void setParams(const Params&);
+
+  /// Processes a single sample through the distortion effect
+  auto processSample(float) const -> float;
+
+private:
+  Params _params;
+};
+} // namespace fsh::fx
