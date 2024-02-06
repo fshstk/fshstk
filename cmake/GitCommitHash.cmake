12485975344eb5b10cb89f75ffce3b80c3eaa898
fshstk <fshstk@fshstk.com>
2024-02-06 23:21:39 +0100

    Cleaner git commit hash function

--- cmake/GitCommitHash.cmake
+++ cmake/GitCommitHash.cmake
@@ -23,28 +23,12 @@
 # (i.e. a release), or to an empty string if it is tagged.
 
 function(get_git_commit_hash GIT_COMMIT_HASH)
-  # Check if the current commit has a tag
   execute_process(
-    COMMAND git describe --exact-match --tags HEAD
+    COMMAND git rev-parse --short HEAD
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    RESULT_VARIABLE GIT_TAG_RESULT
+    OUTPUT_VARIABLE GIT_COMMIT_HASH_LOCAL
     OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET
   )
-
-  # If the command succeeded, there is a tag, so set the commit hash to an empty string
-  # Otherwise, get the commit hash and add parentheses around it
-  if(GIT_TAG_RESULT EQUAL 0)
-    set(${GIT_COMMIT_HASH} "" PARENT_SCOPE)
-    message("Current commit is tagged, no GIT_COMMIT_HASH set")
-  else()
-    execute_process(
-      COMMAND git rev-parse --short HEAD
-      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-      OUTPUT_VARIABLE GIT_COMMIT_HASH_LOCAL
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    set(${GIT_COMMIT_HASH} " (${GIT_COMMIT_HASH_LOCAL})" PARENT_SCOPE)
-    message("GIT_COMMIT_HASH: (${GIT_COMMIT_HASH_LOCAL})")
-  endif()
+  set(${GIT_COMMIT_HASH} ${GIT_COMMIT_HASH_LOCAL} PARENT_SCOPE)
+  message("GIT_COMMIT_HASH: ${GIT_COMMIT_HASH_LOCAL}")
 endfunction()
