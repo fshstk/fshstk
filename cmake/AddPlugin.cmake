2735ab9e434ed500a27efc69010bcf83eff61ddf
fshstk <fshstk@fshstk.com>
2024-02-06 21:02:49 +0100

    Add PRODUCT_NAME

--- cmake/AddPlugin.cmake
+++ cmake/AddPlugin.cmake
@@ -36,6 +36,8 @@ function(fsh_add_plugin)
   endif()
 
   juce_add_plugin(${PROJECT_NAME}
+    # TODO: replace with "fsh :: ${PROJECT_NAME}":
+    PRODUCT_NAME              "${PROJECT_NAME}"
     PLUGIN_CODE               ${FSH_PLUGIN_CODE}
 
     # NEEDS_MIDI_INPUT must be true if IS_SYNTH is true, or AU validation will fail:
