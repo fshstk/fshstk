e3cf9b045fcd30adbaab062685d595b4e6b276a4
fshstk <fshstk@fshstk.com>
2024-02-12 03:38:46 +0100

    Remove VST2 builds
    VST2 is deprecated and may not be distributed without an existing license

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -32,20 +32,6 @@ FetchContent_Declare(
   GIT_TAG        7.0.9)
 FetchContent_MakeAvailable(JUCE)
 
-# In order to build VST2 plug-ins, we need the VST2 SDK which was removed at some point in Juce 5.
-# Here we grab the last commit before the SDK was removed and make it available via the VST_SDK_PATH
-# variable:
-message(STATUS "Fetching VST2 SDK...")
-FetchContent_Declare(
-  JUCE_VST
-  GIT_REPOSITORY https://github.com/juce-framework/JUCE
-  GIT_TAG        a54535bc317b5c005a8cda5c22a9c8d4c6e0c67e)
-FetchContent_GetProperties(JUCE_VST)
-if(NOT juce_vst_POPULATED)
-  FetchContent_Populate(JUCE_VST)
-endif()
-set(VST_SDK_PATH ${juce_vst_SOURCE_DIR}/modules/juce_audio_processors/format_types/VST3_SDK)
-
 ####################################################################################################
 # FMT - C++ String Formatting
 ####################################################################################################
